c15fd218022a22b04db73ddbba948dc77e058d37
Anusha <nssranusha@yahoo.co.in>
2022-06-26 00:10:40 +0530

    first commit of todo app

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.listen(port, function(err) {
     if(err){
-        console.log(`Not up, Error in setting up the server: ${err}`);       
+        console.log(`Error in setting up the server: ${err}`);       
     }
 
     console.log(`Port is up and running : ${port}`);
